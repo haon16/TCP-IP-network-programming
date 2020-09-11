2c4c33fddd43c7211aeedf7b39601ee5a2f3c349
haon1026 <1020090076@qq.com>
2020-09-11 18:15:18 +0800

    add and modify

--- TCPIP网络编程/10.多进程服务器端/5.signal.c
+++ TCPIP网络编程/10.多进程服务器端/5.signal.c
@@ -20,32 +20,32 @@ SIGCHLD :子进程终止
 #include <unistd.h>
 #include <signal.h>
 
-void timeout(int sig)       //分别定义信号处理函数，这种类型的函数称为信号处理器（Handler）
+void timeout(int sig)               //分别定义信号处理函数，这种类型的函数称为信号处理器（Handler）
 {
     if (sig == SIGALRM)
         puts("Time out!");
-    alarm(2);               //每隔2s重复产生SIGALRM信号，在信号处理器中调用alarm函数
+    alarm(2);                       //每隔2s重复产生SIGALRM信号，在信号处理器中调用alarm函数
 }
 
-void keycontrol(int sig)
-{
+void keycontrol(int sig)            //如果在未注册SIGINT信号的情况下输入CTRL+C,将由操作系统默认的事件处理器终止程序
+{                                   //但如果直接注册Ctrl+C信号的处理器，则程序不会终止，而是调用程序员指定的事件处理器
     if (sig == SIGINT)
         puts("CTRL+C pressed");
 }
 
 int main(int argc, char *argv[])
 {
-    signal(SIGALRM, timeout);    //注册SIGALRM,SIGINT信号及相应处理器
+    signal(SIGALRM, timeout);       //注册SIGALRM,SIGINT信号及相应处理器
     signal(SIGINT, keycontrol);
 
-    alarm(2);                   //传递一个正整形参数，相应时间后（以秒为单位）将产生SIGALRM信号。若向该函数传递0，则之前对SIGALRM信号的预约将取消。如果通过该函数
-                                //预约信号后未指定该信号对应的处理函数，则（通过调用signal函数）终止进程，不做任何处理
-                                //返回0或以秒为单位的距SIGALRM信号发生所剩时间
+    alarm(2);                       //传递一个正整形参数，相应时间后（以秒为单位）将产生SIGALRM信号。若向该函数传递0，则之前对SIGALRM信号的预约将取消。如果通过该函数
+                                    //预约信号后未指定该信号对应的处理函数，则（通过调用signal函数）终止进程，不做任何处理
+                                    //返回0或以秒为单位的距SIGALRM信号发生所剩时间
 
     for (int i = 0; i < 3; i++)
     {
-        puts("wait...");         //实际测试没有CTRL+C终止时只输出了2次wait信息，CTRL+C终止时一共输出了3次wait信息，这个没搞明白
-        sleep(100);              //发生信号时将唤醒由于调用sleep函数而进入阻塞状态的进程，一旦被唤醒就不会再进入睡眠状态
+        puts("wait...");            //实际测试没有CTRL+C终止时只输出了2次wait信息，CTRL+C终止时一共输出了3次wait信息，这个没搞明白
+        sleep(100);                 //发生信号时将唤醒由于调用sleep函数而进入阻塞状态的进程，一旦被唤醒就不会再进入睡眠状态
     }
 
     return 0;
